b240502301d3edf319bc517dbe7188c6c6e0a9e4
SantiJaime <santij03@gmail.com>
2025-10-26 17:36:21 -0300

    muevo inicialización de localstorage a index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,35 @@
-// La clave que usaremos para guardar los datos en LocalStorage
-const STORAGE_KEY = 'medicos_idw';
+const initialDoctors = [
+  {
+    id: 1,
+    nombre: "Dr. Juan Pérez",
+    especialidad: "Cardiología",
+    imagen: "../img/doctor4.png",
+  },
+  {
+    id: 2,
+    nombre: "Dra. María Gómez",
+    especialidad: "Pediatría",
+    imagen: "../img/doctora1.png",
+  },
+  {
+    id: 3,
+    nombre: "Dra. Fátima Hernández",
+    especialidad: "Neurología",
+    imagen: "../img/doctora5.jpg",
+  },
+  {
+    id: 4,
+    nombre: "Dr. Pedro Rodríguez",
+    especialidad: "Traumatología",
+    imagen: "../img/doctor3.jpg",
+  },
+  {
+    id: 5,
+    nombre: "Dr. Facundo González",
+    especialidad: "Radiología",
+    imagen: "../img/doctor2.png",
+  },
+];
 
 const obrasSociales = [
   "OSDE",
@@ -8,38 +38,47 @@ const obrasSociales = [
   "GALEANO",
   "SANCOR SALUD",
   "OSPE",
-  "UPCN"
+  "UPCN",
 ];
 
-localStorage.setItem("obrasSociales", JSON.stringify(obrasSociales));
-
 function renderObrasSociales() {
-  const obrasSocialesContainer = document.getElementById("obras-sociales-container");
-  const obrasSocialesLS = JSON.parse(localStorage.getItem("obrasSociales"));
+  const pathName = window.location.pathname;
+  if (pathName !== "/index.html") return;
+  
+  const obrasSocialesContainer = document.getElementById(
+    "obras-sociales-container"
+  );
 
-  obrasSocialesLS.forEach((nombreObraSocial) => {
+  obrasSociales.forEach((nombreObraSocial) => {
     const obraSocialButton = document.createElement("button");
     obraSocialButton.classList.add("button");
     obraSocialButton.textContent = nombreObraSocial;
-    
+
     obrasSocialesContainer.appendChild(obraSocialButton);
   });
 }
 
-/**
- * Función para cargar y mostrar los médicos en la página principal
- */
+function initializeData() {
+  if (!localStorage.getItem("medicos_idw")) {
+    localStorage.setItem("medicos_idw", JSON.stringify(initialDoctors));
+    console.log("Datos de médicos iniciales guardados en LocalStorage.");
+    return;
+  }
+  console.log("Los datos de médicos ya existen en LocalStorage.");
+}
+
 function displayDoctors() {
-    const doctors = JSON.parse(localStorage.getItem(STORAGE_KEY));
-    const doctorsContainer = document.getElementById('doctors-container');
+  const doctors = JSON.parse(localStorage.getItem("medicos_idw"));
+  const doctorsContainer = document.getElementById("doctors-container");
 
-    doctorsContainer.innerHTML = '';
+  doctorsContainer.innerHTML = "";
 
-    if (doctors && doctors.length > 0) {
-        doctors.forEach(doctor => {
-            const card = document.createElement('div');
-            card.className = 'col-lg-3 col-md-6 col-sm-12 py-2 d-flex justify-content-center';
-            card.innerHTML = `
+  if (doctors && doctors.length > 0) {
+    doctors.forEach((doctor) => {
+      const card = document.createElement("div");
+      card.className =
+        "col-lg-3 col-md-6 col-sm-12 py-2 d-flex justify-content-center";
+      card.innerHTML = `
                 <div class="card" style="width: 20rem">
                     <img src="${doctor.imagen}" class="card-img-top" alt="${doctor.nombre}" />
                     <div class="card-body">
@@ -48,13 +87,13 @@ function displayDoctors() {
                     </div>
                 </div>
             `;
-            doctorsContainer.appendChild(card);
-        });
-    }
+      doctorsContainer.appendChild(card);
+    });
+  }
 }
 
-// Llamar a la función de visualización cuando la página cargue
-document.addEventListener('DOMContentLoaded', () => {
-    displayDoctors();
-    renderObrasSociales();
+document.addEventListener("DOMContentLoaded", () => {
+  initializeData();
+  displayDoctors();
+  renderObrasSociales();
 });
